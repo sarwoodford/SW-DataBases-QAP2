e01d04ca996d39ce1cc36b29f13a796855e99ae1
sara woodford <sarawoodford6@gmail.com>
2024-10-09 09:31:04 -0400

    small fix
    mistake in order_items table, fixed to better match
    the rubric

--- OnlineStore.sql
+++ OnlineStore.sql
@@ -20,9 +20,11 @@ customer_id INT REFERENCES customers(id),
 order_date DATE
 );
 
-CREATE TABLE ORDER_ITEMS(
+CREATE TABLE order_items(
 order_id INT REFERENCES orders(id),
-customer_id INT REFERENCES customers(id),
+product_id INT REFERENCES products(id),
 quantity INT,
-PRIMARY KEY(order_id, customer_id)
+PRIMARY KEY(order_id, product_id)
 );
+
+-- INSERT DATA INTO TABLES 
